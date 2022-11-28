68bcca0d2c239d0fb394dde88a1dd06bbc90d3a1
Aleksei Svitoch <pndlm@yandex.ru>
2022-11-28 19:41:03 +0600

    complete task 19

---
+++ task19/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 10000 || num > 99999)
+    Console.Write("Это не пятизначное число ");
+
+else
+{
+    string str = num.ToString();
+   
+    if (str[0] == str[4] && str[1] == str[3])
+        Console.Write("это число является палиндромом");
+    
+    else
+        Console.Write("это число не является палиндромом");
+}
