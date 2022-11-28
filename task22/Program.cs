1a69a6273172302b06ff69e5b44f78a4fd7a6274
Aleksei Svitoch <pndlm@yandex.ru>
2022-11-28 16:56:40 +0600

    complete task 22

---
+++ task22/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+Console.Write($"таблица кубов числа {num}: ");
+while (i <= num)
+{
+    Console.Write($"{Math.Pow(i, 3)} ");
+    i++;
+}
